a6b4906d992c0ccb3548922bb7dce84ced3a316a
chichichi1310 <chisatokyoda@icloud.com>
2025-08-04 19:27:21 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+
+const addBtn = document.getElementById('btn');
+const parentList = document.getElementById('text');
+
+ addBtn.addEventListener('click', () => {
+   parentList.textContent = 'ボタンをクリックしました';
+});
+
+
